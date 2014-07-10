73218945deecaa1db8dce34d0457502bad2117ec
Tom Boutell <tom@punkave.com>
2014-07-09 21:28:23 -0400

    first version

---
+++ README.md
@@ -0,0 +1,24 @@
+# nj
+
+A simple command line for compiling Nunjucks templates to generate static HTML files.
+
+It's *really* easy:
+
+```bash
+npm install --save nj
+nj myfile.nj
+```
+
+That compiles `myfile.nj` and outputs `myfile.html`. Boom!
+
+Since `nj` outputs `.html` files, your nunjucks templates should `not` have a `.html` extension, because you don't want to overwrite them.
+
+You may optionally pass a JSON file as the second argument. If you do, it is parsed and passed to the nunjucks template as data:
+
+```bash
+nj myfile.nj mydata.json
+```
+
+## Changelog
+
+0.1.0: initial release.
---
+++ app.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+var argv = require('yargs').argv;
+var nunjucks = require('nunjucks');
+var path = require('path');
+var fs = require('fs');
+
+if (argv._.length < 1) {
+  usage();
+}
+
+if (argv._.length > 2) {
+  usage();
+}
+
+var input = argv._[0];
+
+if (input.match(/\.html$/)) {
+  console.error('Your input file should NOT be .html, because you');
+  console.error('do not want me to overwrite it. Name it .nj.');
+  usage();
+}
+
+var output = input.replace(/\.\w+$/, '') + '.html';
+var data = {};
+
+if (argv._[1]) {
+  data = JSON.parse(fs.readFileSync(argv._[1], 'utf8'));
+}
+
+fs.writeFileSync(output, nunjucks.render(input, data));
+
+function usage() {
+  console.error('Usage: nj filename.nj [optional JSON filename]');
+  console.error();
+  console.error('The specified nunjucks template is rendered with the');
+  console.error('data found in the specified JSON file, if any, and the');
+  console.error('result written to filename.html.');
+  process.exit(1);
+}
